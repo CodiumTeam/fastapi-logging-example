877794091f4da7e18fec91521e57285dfc0d39f8
Hugo Chinchilla <hugo@codium.team>
2022-11-23 12:36:13 +0100

    enable cors

--- src/server.py
+++ src/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .logging import get_logger
 from .logging_middleware import logging_context, log_request_response
@@ -6,6 +7,13 @@ from .logging_middleware import logging_context, log_request_response
 logger = get_logger(__name__)
 
 app = FastAPI(middleware=[logging_context, log_request_response])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
